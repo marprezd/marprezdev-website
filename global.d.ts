ba774aa5f8b4c416e1b38c9c3a12852fd3396f07
marprezd <marprezd@gmail.com>
2025-11-16 16:46:55 -0500

    :label: feat(types): Add CSS module declaration.

--- global.d.ts
+++ global.d.ts
@@ -2,10 +2,21 @@ import type messages from "./messages/en.json"
 import type { formats } from "@/i18n/request"
 import type { routing } from "@/i18n/routing"
 
+/**
+ * Next-intl configuration
+ */
 declare module "next-intl" {
   interface AppConfig {
     Locale: (typeof routing.locales)[number]
     Messages: typeof messages
     Formats: typeof formats
   }
+}
+
+/**
+ * CSS module declaration
+ */
+declare module "*.css" {
+  const content: { [className: string]: string }
+  export default content
 }
